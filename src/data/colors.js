32216e726cd04f95de7dc23ebf605deb18fe5d1f
alex-fdr <alexfv01@gmail.com>
2025-12-27 21:52:05 +0200

    update formatting

--- src/data/colors.js
+++ src/data/colors.js
@@ -1,6 +1,6 @@
 export const colors = {
-  green: 0x449016,
-  yellow: 0xcaca00,
-  white: 0x999999,
-  pink: 0xb733bf
-}
+    green: 0x449016,
+    yellow: 0xcaca00,
+    white: 0x999999,
+    pink: 0xb733bf,
+};
